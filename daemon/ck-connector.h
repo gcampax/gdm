a5f33e99d06a6f5a8bf74adb0448c20a3f86631d
Giovanni Campagna <gcampagna@src.gnome.org>
2012-05-13 18:43:42 +0200

    Port CkConnector to GDBus
    Port the CkConnector glue code to GLib and GDBus.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=622888

--- daemon/ck-connector.h
+++ daemon/ck-connector.h
@@ -31,9 +31,9 @@
 #define CK_CONNECTOR_H
 
 #include <sys/types.h>
-#include <dbus/dbus.h>
+#include <glib.h>
 
-DBUS_BEGIN_DECLS
+G_BEGIN_DECLS
 
 struct _CkConnecter;
 typedef struct _CkConnector CkConnector;
@@ -43,22 +43,22 @@ CkConnector  *ck_connector_new                          (void);
 CkConnector  *ck_connector_ref                          (CkConnector *ckc);
 void          ck_connector_unref                        (CkConnector *ckc);
 
-dbus_bool_t   ck_connector_open_session_for_user        (CkConnector *ckc,
+gboolean      ck_connector_open_session_for_user        (CkConnector *ckc,
                                                          uid_t        user,
                                                          const char  *tty,
                                                          const char  *x11_display,
-                                                         DBusError   *error);
-dbus_bool_t   ck_connector_open_session_with_parameters (CkConnector *ckc,
-                                                         DBusError   *error,
+                                                         GError     **error);
+gboolean      ck_connector_open_session_with_parameters (CkConnector *ckc,
+                                                         GError     **error,
                                                          const char  *first_parameter_name,
                                                          ...);
-dbus_bool_t   ck_connector_open_session                 (CkConnector *ckc,
-                                                         DBusError   *error);
+gboolean      ck_connector_open_session                 (CkConnector *ckc,
+                                                         GError     **error);
 
 const char   *ck_connector_get_cookie                   (CkConnector *ckc);
-dbus_bool_t   ck_connector_close_session                (CkConnector *ckc,
-                                                         DBusError   *error);
+gboolean      ck_connector_close_session                (CkConnector *ckc,
+                                                         GError     **error);
 
-DBUS_END_DECLS
+G_END_DECLS
 
 #endif /* CK_CONNECTOR_H */
